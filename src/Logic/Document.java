1e9a132cfb149b8b115a7e4d0f9b231f4be7c4f4
Nick <nmarko6@gmail.com>
2021-11-30 18:36:18 -0500

    added method to compare images with opencv

--- src/Logic/Document.java
+++ src/Logic/Document.java
@@ -11,6 +11,7 @@ import javax.imageio.ImageIO;
 
 import java.awt.image.BufferedImage;
 import java.util.ArrayList;
+import java.awt.Color;
 
 import org.apache.pdfbox.cos.COSName;
 import org.apache.pdfbox.pdmodel.PDDocument;
@@ -129,4 +130,49 @@ public class Document extends File {
 		}
 	}
 
+	public boolean compareImages() throws IOException {
+		//replace with user image
+		BufferedImage img1 = ImageIO.read(new File("D:\\Images\\test1.jpg"));
+		//replace with document image
+      	BufferedImage img2 = ImageIO.read(new File("D:\\Images\\test2.jpg"));
+		  
+     	int w1 = img1.getWidth();
+     	int w2 = img2.getWidth();
+        int h1 = img1.getHeight();
+        int h2 = img2.getHeight();
+        if ((w1!=w2)||(h1!=h2)) {
+           System.out.println("Both images should have same dimensions");
+		   return false;
+        } else {
+           long diff = 0;
+           for (int j = 0; j < h1; j++) {
+              for (int i = 0; i < w1; i++) {
+                 //Getting the RGB values of a pixel
+                 int pixel1 = img1.getRGB(i, j);
+                 Color color1 = new Color(pixel1, true);
+                 int r1 = color1.getRed();
+                 int g1 = color1.getGreen();
+                 int b1 = color1.getBlue();
+                 int pixel2 = img2.getRGB(i, j);
+                 Color color2 = new Color(pixel2, true);
+                 int r2 = color2.getRed();
+                 int g2 = color2.getGreen();
+                 int b2= color2.getBlue();
+                 //sum of differences of RGB values of the two images
+                 long data = Math.abs(r1-r2)+Math.abs(g1-g2)+ Math.abs(b1-b2);
+                 diff = diff+data;
+              }  
+           }
+          double avg = diff/(w1*h1*3);
+          double percentage = (avg/255)*100;
+          System.out.println("Difference: "+percentage);
+
+		//if similarity is above 70% then it's a match (can change easily)
+		  if (percentage >= 70) {
+			    return true;
+		  } else {
+			    return false;
+		  }
+      }			
+	}
 }
